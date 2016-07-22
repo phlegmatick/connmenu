bd25223624e46e30377149ce0d13deb728210b06
phlegmatick <phlegmatick@lambdacore.org>
2016-07-21 23:01:49 -0500

    Fix label case and the exit options

--- connmenu.py
+++ connmenu.py
@@ -2,7 +2,7 @@
 '''
 ConnMenu: a simple SSH connection menu
 Author: Blake Buckalew
-Version: 0
+Version: 1
 Description: A simple console-based SSH connection menu, fed by an INI style configuration file
 Usage: Just execute it. Optionally provide a configuration file as an argument.
 Dependencies: cursesmenu (pip install curses-menu)
@@ -21,30 +21,42 @@ from pprint import pprint
 from cursesmenu import *
 from cursesmenu.items import *
 
+exit_string = 'ConnMenu exiting...'
+
 config_file = os.path.expanduser('~/connmenu.conf')
 
 config_file = sys.argv[1] if (len(sys.argv) > 1) else config_file
 print config_file
 hosts = ConfigParser.ConfigParser()
+hosts.optionxform=str # preserve case in conn labels
 hosts.read(config_file)
 
 categories = hosts.sections()
 
 #proceed = raw_input('\nPress enter to proceed.')
 
-category_selection = SelectionMenu.get_selection( categories )
+category_selection = SelectionMenu.get_selection( categories , 'Connection Categories')
 
-category_name = categories[category_selection]
-
+try:
+	category_name = categories[category_selection]
+except IndexError:
+    print exit_string
+    sys.exit()
+	
 servers = hosts.items(category_name)
 
 pprint(servers)
 
 #proceed = raw_input('\nPress enter to proceed.')
 
-selection = SelectionMenu.get_selection( [ s[0] for s in servers ] )
+selection = SelectionMenu.get_selection( [ s[0] for s in servers ] , category_name )
 
-print servers[selection]
+try:
+    print servers[selection]
+except IndexError:
+    print exit_string
+    sys.exit()
+
 label = servers[selection][0]
 
 serverconf = servers[selection][1].split(',')
@@ -60,4 +72,4 @@ print command
 
 # Launch interactive SSH
 subprocess.call(command, shell=True)
-print 'ssh exited. ConnMenu exiting...'
+print 'ssh exited.', exit_string
