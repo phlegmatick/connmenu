c3ff52c10d0994ee9d35db3e988f2130bb0a38cb
phlegmatick <phlegmatick@lambdacore.org>
2017-01-08 20:27:27 -0600

    Add reconnect option after SSH exit

--- connmenu.py
+++ connmenu.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-'''
+"""
 ConnMenu: a simple SSH connection menu
 Author: Blake Buckalew
 Version: 1
@@ -14,12 +14,14 @@ Configuration example:
 	[Production]
 	prod01:10.10.10.11,user,pass
 	prod02:10.10.10.12,user,hunter2
-'''
+"""
 import ConfigParser, sys, subprocess
 import os
 from pprint import pprint
 from cursesmenu import *
 from cursesmenu.items import *
+
+connMenu = True
 
 # title settings
 title = True # set terminal title to $hostLabel ($username)
@@ -28,13 +30,14 @@ exit_string = 'ConnMenu exiting...'
 
 config_file = os.path.expanduser('~/connmenu.conf')
 
+config_file = sys.argv[1] if (len(sys.argv) > 1) else config_file
+print config_file
+
 if not os.path.isfile(config_file):
     print "Configuration file not found!"
     print exit_string
     sys.exit()
 
-config_file = sys.argv[1] if (len(sys.argv) > 1) else config_file
-print config_file
 hosts = ConfigParser.ConfigParser()
 hosts.optionxform=str # preserve case in conn labels
 hosts.read(config_file)
@@ -74,13 +77,21 @@ print 'serverconf', serverconf
 host = serverconf[0]
 username, password = serverconf[1].strip(), serverconf[2]
 
-print "Connecting to %s..." % host
-command = "ssh '%s' -l %s" % ( host, username)
-print command
+connMenu = True
+while connMenu:
+	
+	print "Connecting to %s..." % host
+	command = "ssh '%s' -l %s" % ( host, username)
+	print command
 
-if title:
-    subprocess.call("xtitle \"%s (%s)\"" % (host.split('.')[0].strip(), username), shell=True)
+	if title:
+		subprocess.call("xtitle \"%s (%s)\"" % (host.split('.')[0].strip(), username), shell=True)
 
-# Launch interactive SSH
-subprocess.call(command, shell=True)
-print 'ssh exited.', exit_string
+	# Launch interactive SSH
+	subprocess.call(command, shell=True)
+
+	# Exit logic
+	exitOption = raw_input('\nSSH exited. [R]econnect or any other key to exit: ')
+
+	if exitOption.strip().upper() != 'R':
+		sys.exit()
